c925eaa31bedebe087e59a1c48e072546fa49814
Vincent Chang <vntchang@gmail.com>
2024-03-19 23:43:19 +0800

    Modify gitignore

--- dist/board.js
+++
@@ -1,2 +0,0 @@
-"use strict";var typedoc=require("typedoc");exports.load=function(app){app.options.addDeclaration({name:"plugin-option",help:"Displayed when --help is passed",type:typedoc.ParameterType.String,defaultValue:""}),app.converter.on(typedoc.Converter.EVENT_RESOLVE,(context=>{app.options.getValue("plugin-option")}))};
-//# sourceMappingURL=board.js.map
